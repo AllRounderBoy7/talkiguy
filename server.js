762a01cd55b3f977c310f72919dfc0d1874797ca
AllRounderBoy7 <sahanapraveen2006@gmail.com>
2025-07-28 21:57:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
-
+const PORT = process.env.PORT || 10000;
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
